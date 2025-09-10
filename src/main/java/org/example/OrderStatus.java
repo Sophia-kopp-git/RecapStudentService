7bbd0e3c41212d290f0a7ac45fa007198d4af332
sophiakopp <sophiakopp@gmx.de>
2025-09-10 11:03:37 +0200

    add enum order status

---
+++ src/main/java/org/example/OrderStatus.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public enum OrderStatus {
+    PROCESSING ("processing"),
+    IN_DELIVERY ("in delivery"),
+    COMPLETED ("completed");
+
+    private final String value;
+
+    OrderStatus(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
